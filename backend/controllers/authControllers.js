fa14d5156162fbd1f16b29ec6a2c4f5cdf284ef0
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-07-01 20:38:00 +0530

    created user model

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,45 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateVerificationCode } from "../utils/generateVerificationCode.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
+
 export const signUp = async (req, res) => {
-  res.send("SignUp Route");
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exist" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken=generateVerificationCode();
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, //24hrs
+      verificationToken,
+    });
+    await user.save();
+
+    //jwt
+    generateTokenAndSetCookie(res,user._id)
+    res.status(200).json({success:true,message:"Sign Up successful",
+      user:{
+        ...user._doc,
+        password:undefined
+      }
+    })
+
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 import express from "express";
-
+import dotenv from "dotenv";
 //
 import { connectDB } from "./config/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 //
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("hello World");
-});
-
+app.use(express.json()); //allows us to parse incoming requests from req.body
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log(
-    "server is listening on port 3000 and use url http://localhost:3000"
+    `server is listening on port ${PORT} and use url http://localhost:${PORT}`
   );
 });
