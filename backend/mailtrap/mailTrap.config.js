74eda18448f8f6309aa26a0b32ae916eb28ff2f5
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-07-01 21:06:40 +0530

    connected mailtrap

---
+++ backend/mailtrap/mailTrap.config.js
@@ -0,0 +1,15 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.co",
+  name: "Abhiram Vakil",
+};
